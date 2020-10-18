e1b9481661ddb704b89e51ea44795d7e5708e348
Deanna Johnson <deannaj401@gmail.com>
2020-10-17 18:17:22 -0600

    end of day

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 // //Declare port
 const PORT = process.env.PORT || 3000;
@@ -13,66 +14,88 @@ const app = express();
 
 // //Use CORS
 app.use(cors());
+//express is able to read postman
+app.use(express.urlencoded());
 // //start the server
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
 
-  response.send(location);
+  res.send("hi there");
 });
 
 
 
-function Location(obj, query) {
-  this.latitude = obj[0].lat;
-  this.longitude = obj[0].lon;
-  this.search_query = query;
-  this.formatted_query = obj[0].display_name;
+function Location(city, location) {
+  this.latitude = location.lat;
+  this.longitude = location.lon;
+  this.search_query = city;
+  this.formatted_query = location.display_name;
 }
 
-function Weather(forecast, date) {
+function Weather(forecast, time) {
   this.forecast = forecast;
-  this.date = date;
+  this.time = time;
 
 
 }
 
 app.get('/location', getLocation);
-app.get('/weather', getWeather);
+app.get('weather', getWeather);
+app.use('*', notFoundHandler);
 
 //get the information
-function getLocation(request, response) {
-  try {
-    let data = require('./data/location.json');
-    let city = request.query.city;
-    console.log(city);
-    let location = new Location(data, city);
-    console.log(location);
-    //determining that all is working
-    response.status(200).json(location);
-  }
-  catch (error) {
-    console.log('ERROR', error );
-    response.status(500).json("So Sorry, something went terribly wrong!");
+function getLocation(req, res) {
+  let city = req.query.city;
+  // console.log(city);
+  let key = process.env.LOCATIONAPIKEY;
+  const URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+  console.log(URL);
+
+  superagent.get(URL)
+    .then(data => {
+      let location = new Location(city, data.body[0]);
 
 
-  }
+      //determining that all is working
+      res.status(200).json(location);
+    })
+    .catch(error => {
+      console.log('ERROR', error);
+      res.status(500).send('So Sorry, something went terribly wrong!');
+    });
+
 }
 
-function getWeather(request, response) {
-  let weatherArray = [];
-  let data = require('./data/weather.json');
-  data.data.forEach(element => {
+
+function getWeather(req, res) {
+  let weatherArray = data.data.map(element => {
     let forecast = element.weather.description;
-    let date = element.valid_date;
-    let weather = new Weather(forecast, date);
-    weatherArray.push(weather);
+    let time = element.datetime;
+
+    let key = process.env.WEATHERAPIKEY;
+    const URL = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${key}`;
+    // console.log(URL);
+
+    res.status(200).json(location);
+
+  });
+}
+
+
+superagent.get(URL)
+  .then(data => {
+    let weather = new Weather(forecast, time);
+    //determining that all is working
+    res.status(200).json(weatherArray);
+
   });
 
-  console.log(data.data[0]);
-  //determining that all is working
-  response.status(200).json(weatherArray);
+function notFoundHandler(req, res) {
+  res.status(404).send('Page Not Found');
 
 }
 
 app.listen(PORT, () => {
   console.log(`Server is now listening on port ${PORT}`);
+
+
 });
